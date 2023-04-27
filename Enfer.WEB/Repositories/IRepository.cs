8100e28133324cfc5dfc3d369b614a095f4910df
felitow <juanfelipeblancoorrego@gmail.com>
2023-04-26 23:20:52 -0500

    Ayuda

---
+++ Enfer.WEB/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Enfer.WEB.Repositories;
+
+namespace Enfer.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+    }
+}
