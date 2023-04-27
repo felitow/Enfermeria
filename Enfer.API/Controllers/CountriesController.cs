3d11d696b8b3ec3091a548059e0403fd6fdf3cde
felitow <juanfelipeblancoorrego@gmail.com>
2023-04-26 22:52:00 -0500

    Creación País

---
+++ Enfer.API/Controllers/CountriesController.cs
@@ -0,0 +1,34 @@
+﻿using Enfer.API.Data;
+using Enfer.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enfer.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+    }
+}
