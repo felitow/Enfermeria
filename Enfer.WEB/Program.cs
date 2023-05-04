01010fde827986b09a3da5a3b2c902f386e0b023
felitow <juanfelipeblancoorrego@gmail.com>
2023-05-04 10:37:27 -0500

    Arreglo y Funcional

--- Enfer.WEB/Program.cs
+++ Enfer.WEB/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7161/")});
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7162/")});
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
 
