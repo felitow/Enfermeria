c1a5bf75f976bd635c9ea634d2634103f5d3ea05
felitow <juanfelipeblancoorrego@gmail.com>
2023-05-05 00:01:39 -0500

    h

--- Enfer.WEB/Program.cs
+++ Enfer.WEB/Program.cs
@@ -1,15 +1,25 @@
+using BlazorStrap;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Enfer.WEB;
 using Enfer.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7162/")});
-builder.Services.AddScoped<IRepository, Repository>();
-builder.Services.AddSweetAlert2();
 
-await builder.Build().RunAsync();
+
+    
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7162/") });
+        //builder.Services.AddBootstrapCss();
+        builder.Services.AddScoped<IRepository, Repository>();
+        builder.Services.AddSweetAlert2();
+
+        //builder.Services.AddBlazorStrap();
+        //builder.Services.AddBootstrapCss();
+
+        await builder.Build().RunAsync();
+    
